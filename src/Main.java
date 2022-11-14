7d8a129841e967651e1ec6f1aed202d7a1037219
Andrey Zinchenko <astrokoit@gmail.com>
2022-11-14 22:59:44 +0700

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+class Print2Task {
+    public static void printArr(String[]  arr) {
+        for(String i : arr)
+            System.out.print(!i.equals(arr[arr.length - 1]) ? i + ", " : i + "\n");
+    }
+
+    public static void printArr(int[] arr) {
+        for(Object i : arr)
+            System.out.print(!i.equals(arr[arr.length - 1]) ? i + ", " : i + "\n");
+    }
+
+    public static void printArr(double[] arr) {
+        for(Object i : arr)
+            System.out.print(!i.equals(arr[arr.length - 1]) ? i + ", " : i + "\n");
+    }
+
+    public static void printArrBackwards(String[] arr) {
+        for(int i = arr.length - 1; i >= 0; i--)
+            System.out.print( i != 0 ? arr[i] + ", " : arr[i] + "\n");
+    }
+
+    public static void printArrBackwards(int[] arr) {
+        for(int i = arr.length - 1; i >= 0; i--)
+            System.out.print( i != 0 ? arr[i] + ", " : arr[i] + "\n");
+    }
+
+    public static void printArrBackwards(double[] arr) {
+        for(int i = arr.length - 1; i >= 0; i--)
+            System.out.print( i != 0 ? arr[i] + ", " : arr[i] + "\n");
+    }
+}
+
 public class Main {
+    public static void task1To3() {
+        int[] firstArr = new int[3];
+        firstArr[0] = 1;
+        firstArr[1] = 2;
+        firstArr[2] = 3;
+
+        double[] secondArr = {1.57, 7.654, 9.986};
+        String[] seasons  = {"Winter", "Spring", "Summer", "Autumn"};
+
+        Print2Task.printArr(firstArr);
+        Print2Task.printArr(secondArr);
+        Print2Task.printArr(seasons);
+
+        System.out.println("\nОбратный вывод: \n");
+
+        Print2Task.printArrBackwards(firstArr);
+        Print2Task.printArrBackwards(secondArr);
+        Print2Task.printArrBackwards(seasons);
+    }
+
+    public static int[] task4(int[] arr){
+        for(int i = 0; i < arr.length; i++)
+            if (arr[i] % 2 != 0) arr[i] += 1;
+        return arr;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1To3();
     }
 }
